bb422f27493434bbd770b2f36f3c49072a31bfd6
Alessio Lei <alelei94@yahoo.it>
2024-03-07 12:53:33 +0100

    Adds to_hex_ncs function to version.cmake

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,7 +1,34 @@
+
+function(to_hex_ncs DEC HEX)
+  if(DEC EQUAL 0)
+    set(${HEX} "0x0" PARENT_SCOPE)
+    return()
+  endif()
+  while(DEC GREATER 0)
+    math(EXPR _val "${DEC} % 16")
+    math(EXPR DEC "${DEC} / 16")
+    if(_val EQUAL 10)
+      set(_val "A")
+    elseif(_val EQUAL 11)
+      set(_val "B")
+    elseif(_val EQUAL 12)
+      set(_val "C")
+    elseif(_val EQUAL 13)
+      set(_val "D")
+    elseif(_val EQUAL 14)
+      set(_val "E")
+    elseif(_val EQUAL 15)
+      set(_val "F")
+    endif()
+    set(_res "${_val}${_res}")
+  endwhile()
+  set(${HEX} "0x${_res}" PARENT_SCOPE)
+endfunction()
+
 math(EXPR NCS_VERSION_CODE "(${NCS_VERSION_MAJOR} << 16) + (${NCS_VERSION_MINOR} << 8)  + (${NCS_VERSION_PATCH})")
 
 # to_hex is made available by ${ZEPHYR_BASE}/cmake/hex.cmake
-to_hex(${NCS_VERSION_CODE} NCS_VERSION_NUMBER)
+to_hex_ncs(${NCS_VERSION_CODE} NCS_VERSION_NUMBER)
 
 if(DEFINED BUILD_VERSION)
   set(ncs_banner_version BUILD_VERSION)
@@ -30,3 +57,4 @@ add_custom_command(
 add_custom_target(ncs_version_h DEPENDS ${PROJECT_BINARY_DIR}/include/generated/ncs_version.h)
 
 add_dependencies(version_h ncs_version_h)
+
